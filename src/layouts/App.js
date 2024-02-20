7d6ba10405e5defbdb500997b09864403a7464d6
Damian Ożóg <dam.ozog@gmail.com>
2024-02-20 20:02:58 +0100

    add Footer.js and Footer.css some changes in styles

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -3,11 +3,13 @@ import React, { Component } from "react";
 import "../styles/App.css";
 import "../styles/Navigation.css";
 import "../styles/Projekty.css";
+import '../styles/Footer.css'
 
 import Navigation from "./Navigation";
 import Welcome from "./Welcome";
 import Aboutme from "./Aboutme";
 import Project from "./Project";
+import Footer from "./Footer";
 
 class App extends Component {
 	state = {};
@@ -18,6 +20,7 @@ class App extends Component {
 				<main>{<Welcome />}</main>
 				<section>{<Aboutme />}</section>
 				<section className='project'>{<Project />}</section>
+				<footer>{<Footer />}</footer>
 			</div>
 		);
 	}
---
+++ src/layouts/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return ( 
+        <div className="footer">
+            <span>STOPKA</span>
+        </div>
+     );
+}
+ 
+export default Footer;
