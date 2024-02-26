0a256da380ba583f9b90286d34262d2dae2fab5c
Damian Ożóg <dam.ozog@gmail.com>
2024-02-26 21:22:16 +0100

    changes in styles

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Navigation = () => {
   const email = (
-    <a href="mailto:dam.ozog@gmail.com" target="_blank" alt="email">
+    <a href="mailto:dam.ozog@gmail.com" target="_blank" alt="email" rel="noreferrer">
       <i className="fa-solid fa-envelope"></i>
     </a>
   );
 
   const github = (
-    <a href="https://github.com/dam-ozog" target="_blank">
+    <a href="https://github.com/dam-ozog" target="_blank" rel="noreferrer">
       <i className="fa-brands fa-square-github"></i>
     </a>
   );
@@ -24,16 +24,16 @@ const Navigation = () => {
           <ul>
             <li>{aboutMe}</li>
             <li>{projects}</li>
-            <li>Three</li>
           </ul>
         </nav>
       </div>
-      <div className="kontakt">
-        <ul className="kontakt-ul">
-          <li>{email}</li>
-          <li>{github}</li>
-          <li>3</li>
-        </ul>
+      <div className="nav">
+        <nav className="nav-ul">
+          <ul className="ul-2">
+            <li>{email}</li>
+            <li>{github}</li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
