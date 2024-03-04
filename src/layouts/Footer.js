48d9624e5c44fb5dc1ac6bc7dca324ecdbe14453
Damian Ożóg <dam.ozog@gmail.com>
2024-03-04 13:21:45 +0100

    footer and mediaqueres rework

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,11 +1,26 @@
-
-
 const Footer = () => {
-    return ( 
-        <div className="footer">
-            <span>STOPKA</span>
-        </div>
-     );
-}
- 
+  const email = (
+    <a href="mailto:dam.ozog@gmail.com" target="_blank" alt="email" rel="noreferrer">
+      <i className="fa-solid fa-envelope"></i>
+      <span>Email</span>
+    </a>
+  );
+
+  const github = (
+    <a href="https://github.com/dam-ozog" target="_blank" rel="noreferrer">
+      <i className="fa-brands fa-square-github"></i>
+      <span>Github</span>
+    </a>
+  );
+
+  return (
+    <section className="footer-section">
+      <div className="footer-container">
+        <div className="contact">{email}</div>
+        <div className="contact">{github}</div>
+      </div>
+    </section>
+  );
+};
+
 export default Footer;
--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,16 +1,5 @@
 const Navigation = () => {
-  const email = (
-    <a href="mailto:dam.ozog@gmail.com" target="_blank" alt="email" rel="noreferrer">
-      <i className="fa-solid fa-envelope"></i>
-    </a>
-  );
-
-  const github = (
-    <a href="https://github.com/dam-ozog" target="_blank" rel="noreferrer">
-      <i className="fa-brands fa-square-github"></i>
-    </a>
-  );
-
+  
   const aboutMe = <a href="#aboutme">About</a>;
 
   const projects = <a href="#projects">Projects</a>;
